ba35850e8f692b3bfc7368fd63c60a6edb159849
eman01234 <danattewodros@gmail.com>
2024-07-10 01:29:58 -0700

    my first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
 function App() {
   return (
     <div className="bg-yellow-500 h-screen">
-      <div className="pt-2 flex text-center font-bold">Hello there world</div>
+      <div className="pt-2 flex text-center font-bold">
+        Hello there world
+        <div>hellow</div>
+        <div>hellow</div>
+        <div>hellow</div>
+        <div>hellow</div>
+        <div>hellow</div>
+        <div>hellow</div>
+      </div>
     </div>
   );
 }
