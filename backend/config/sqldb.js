e86cc83e815daaf9296483b606cda719b8ca2f2a
eman01234 <danattewodros@gmail.com>
2024-07-30 11:37:32 +0300

    b

--- backend/config/sqldb.js
+++ backend/config/sqldb.js
@@ -1,27 +1,27 @@
-const mysql = require("mysql");
+// const mysql = require("mysql");
 
-const connectDatabase = async () => {
-  try {
-    // MySQL Connection
-    const db = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "password",
-      database: "AtkltDB",
-    });
+// const connectDatabase = async () => {
+//   try {
+//     // MySQL Connection
+//     const db = mysql.createConnection({
+//       host: "localhost",
+//       user: "root",
+//       password: "password",
+//       database: "AtkltDB",
+//     });
 
-    // Connect to MySQL
-    db.connect((err) => {
-      if (err) {
-        console.error("Error connecting to MySQL: " + err.stack);
-        return;
-      }
-      console.log("Connected to MySQL as ID " + db.threadId);
-    });
-  } catch (error) {
-    console.error("Error connecting to MySQL: " + error.stack);
-    return;
-  }
-};
+//     // Connect to MySQL
+//     db.connect((err) => {
+//       if (err) {
+//         console.error("Error connecting to MySQL: " + err.stack);
+//         return;
+//       }
+//       console.log("Connected to MySQL as ID " + db.threadId);
+//     });
+//   } catch (error) {
+//     console.error("Error connecting to MySQL: " + error.stack);
+//     return;
+//   }
+// };
 
-module.exports = connectDatabase;
+// module.exports = connectDatabase;
